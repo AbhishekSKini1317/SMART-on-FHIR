cfc92ad8df9ff77c052a085054728f2a4a6d13da
AbhishekSKini1317 <abhishek@zoftsolutions.com>
2022-09-02 15:27:29 +0530

    resolving null issue

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ async function userRequests() {
   }).then(function(data) {
     return data
   }): null;
-  var userResponse = userDetails ? await userDetails.json() : 'Nil';
+  var userResponse = userDetails ? await userDetails.json() : '';
   console.log(userResponse)
   var firstName = userResponse.name ? (userResponse.name[0].given || 'Nil') : 'Nil';
   var lastName = userResponse.name ? (userResponse.name[0].family || 'Nil') : 'Nil';
